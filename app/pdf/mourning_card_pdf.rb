d98c5f93cb89833bbb4680cb690f6ef1696f1731
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2023-06-15 22:45:38 +0200

    apply styling to mourning card pdf

--- app/pdf/mourning_card_pdf.rb
+++ app/pdf/mourning_card_pdf.rb
@@ -2,24 +2,43 @@ require 'prawn'
 
 class MourningCardPdf < Prawn::Document
   def initialize(mourning_card)
-    super()
+    super(page_size: 'A4', margin: [50, 75])
     @mourning_card = mourning_card
     @funeral = mourning_card.funeral
+
+    font_families.update("Vivaldi" => {
+      normal: Rails.root.join('app/assets/fonts/vivaldi.ttf')
+    })
+
     generate_pdf
   end
-  
+
   def generate_pdf
-    text "Mourning Card", size: 30, style: :bold
+    # Header
+    move_down 20
+    text "Mourning Card", size: 26, style: :bold, align: :center
     move_down 20
 
-    text "Description:", style: :bold
-    text @mourning_card.description
+    move_down 30
+    font "Vivaldi" do
+      text "In Loving Memory", size: 22, align: :center
+    end
     move_down 20
 
-    text "Funeral Information:", style: :bold
-    text "Deceased: #{@funeral.deceased.first_name} #{@funeral.deceased.last_name}"
-    text "Burial Place: #{@funeral.burial_place}"
-    text "Funeral Date: #{@funeral.funeral_date.strftime('%B %d, %Y')}"
-    
+    text "#{@funeral.deceased.first_name} #{@funeral.deceased.last_name}", size: 20, style: :italic, align: :center
+    move_down 20
+
+    font "Vivaldi" do
+      text @mourning_card.description, size: 14, align: :center
+    end
+
+    move_down 200
+
+    text "Funeral Information", size: 18, style: :bold, align: :center
+    move_down 30
+
+    text "Burial Place: #{@funeral.burial_place}", size: 14, align: :center
+    text "Funeral Date: #{@funeral.funeral_date.strftime('%B %d, %Y')}", size: 14, align: :center
+
   end
 end

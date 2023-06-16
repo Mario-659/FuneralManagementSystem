7c0f8cf44050e96f23c6f2a5651cdde9de38d8d5
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2023-06-16 17:56:01 +0200

    add ids in forms as selector

--- app/models/deceased.rb
+++ app/models/deceased.rb
@@ -1,3 +1,7 @@
 class Deceased < ApplicationRecord
   belongs_to :user
+
+  def display_name
+    "#{first_name} #{last_name} (Date of Birth: #{date_of_birth})"
+  end
 end
--- app/models/funeral.rb
+++ app/models/funeral.rb
@@ -1,4 +1,8 @@
 class Funeral < ApplicationRecord
   belongs_to :deceased
   enum burial_type: { traditional: 0, cremation: 1, green_burial: 2 }
+
+  def display_name
+    "#{deceased.first_name} #{deceased.last_name} (Funeral Date: #{funeral_date})"
+  end
 end

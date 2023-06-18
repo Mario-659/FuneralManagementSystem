b056f94de3bf6cbf280a54cfb97279b6bc74269c
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2023-06-18 16:21:31 +0200

    remove redundant property from validation

--- app/models/funeral.rb
+++ app/models/funeral.rb
@@ -2,7 +2,7 @@ class Funeral < ApplicationRecord
   belongs_to :deceased
   enum burial_type: { traditional: 0, cremation: 1, green_burial: 2 }
 
-  validates :funeral_date, :funeral_time, :burial_type, :deceased_id, :burial_place, :reception_location, presence: true
+  validates :funeral_date, :burial_type, :deceased_id, :burial_place, :reception_location, presence: true
   validates :burial_place, :reception_location, length: { minimum: 2, maximum: 50 }
   validates :burial_type, inclusion: { in: burial_types.keys }
 

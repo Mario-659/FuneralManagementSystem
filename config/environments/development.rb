0737d2deee1a6131c7f3e59f537284fc434e6d05
Damian Lukasiewicz <damian.lukasiewicz99@gmail.com>
2023-06-18 15:03:23 +0200

    fix sending mails in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,5 +68,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.default_url_options = { host: 'funeral-service.onrender.com' }
+  config.action_mailer.default_url_options = { host: 'dev.funeral-service.com' }
 end
